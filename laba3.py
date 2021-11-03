4baaa457c3ea6150f689a7889ce461fb5f3430b1
cutcutcutcut <bekhterev98@mail.ru>
2021-11-03 22:06:38 +0400

    add Xes

--- laba3.py
+++ laba3.py
@@ -1,11 +1,30 @@
 import numpy as np
 
+x_matrix_2 = np.array(([[1, 1]]))
+
+x_matrix_3 = np.array([[1, 1, 1],
+                       [1, 1, 0],
+                       [1, 0, 1],
+                       [0, 1, 1]])
+
+x_matrix_4 = np.array([[0, 1, 0, 1],
+                       [0, 1, 1, 1],
+                       [0, 0, 1, 1],
+                       [1, 0, 1, 1],
+                       [1, 0, 1, 0],
+                       [1, 1, 0, 1],
+                       [1, 0, 0, 1],
+                       [1, 1, 0, 0],
+                       [0, 1, 1, 0],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 0]])
+
 # Task 3.1
-def make_G():
+#def make_G():
 
 
 
-def make_H():
+#def make_H():
 
 
 
@@ -14,13 +33,14 @@ def make_H():
 
 
 # Task 3.3
-def make_G2():
+#def make_G2():
 
 
-def make_H2():
+#def make_H2():
 
 
 
 # Task 3.4
 
 if __name__ == '__main__':
+ print("3.1")
