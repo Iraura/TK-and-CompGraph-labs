180b87db5d290121db4fd7d92461167bbf3fdabe
Iraura <irauraltseva@gmail.com>
2021-11-03 20:10:21 +0400

    lr3

---
+++ laba3.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# Task 3.1
+def make_G():
+
+
+
+def make_H():
+
+
+
+
+# Task 3.2
+
+
+# Task 3.3
+def make_G2():
+
+
+def make_H2():
+
+
+
+# Task 3.4
+
+if __name__ == '__main__':
