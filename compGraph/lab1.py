c3d41a9d907a13cdd063fc0a97771020531e3ff1
PavelPimKot <pimukov.pavel@gmail.com>
2022-02-08 08:38:10 +0400

    created black square

--- compGraph/lab1.py
+++ compGraph/lab1.py
@@ -1,5 +1,12 @@
+from PIL import Image
+import numpy as np
 
+
+def task1(w, h):
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    img = Image.fromarray(data, 'RGB')
+    img.show()
 
 
 if __name__ == '__main__':
-
+    task1(512, 512)
