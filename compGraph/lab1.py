62f3617f57a1a17019d09da57cadad0053e37757
Egor Novichkov <61648756+spelldone@users.noreply.github.com>
2022-04-11 23:06:47 +0400

    ref

--- compGraph/lab1.py
+++ compGraph/lab1.py
@@ -14,14 +14,13 @@ class Picture:
     h = 512
     w = 512
     picture_array = np.zeros((h, w, 3), dtype=np.uint8)
-    default_colour = [0, 0, 0]
+    default_colour = Colour([0, 0, 0])
 
     def __init__(self, h, w, col: Colour):
         self.h = h
         self.w = w
         self.picture_array = np.zeros((h, w, 3), dtype=np.uint8)
-        self.default_colour = col.colour_array
-        self.picture_array[0:h, 0:w] = col.colour_array
+        self.default_colour = col
         self.z_matrix = np.zeros((h, w))
 
     def create_from_array(self, array):
@@ -37,6 +36,122 @@ class Picture:
 
     def clear(self):
         self.picture_array[0:self.h, 0:self.w] = self.default_colour
+
+    # 4-ый вариант отрисовки линий (алгоритм Брезенхема)
+    def line_v_4(self, x1, y1, x0, y0, colour: Colour, delts_t=0.0):
+        sleep = False
+        if abs(x0 - x1) < abs(y0 - y1):
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+            sleep = True
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        d_x = x1 - x0
+        d_y = y1 - y0
+        d_error = abs(d_y / float(d_x))
+        error = 0.
+        y = y0
+
+        for x in range(int(x0), int(x1) + 1):
+            if sleep:
+                self.set_pixel(y, x, colour)
+            else:
+                self.set_pixel(x, y, colour)
+            error += d_error
+            if error > 0.5:
+                if y1 > y0:
+                    y += 1
+                else:
+                    y -= 1
+                error -= 1.
+
+    # 3-ий вариант отрисовки линий
+    def line_v_3(self, x1, y1, x0, y0, colour: Colour, delta_t=0.0):
+        sleep = False
+        x = x0
+        if abs(x0 - x1) < abs(y0 - y1):
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+            sleep = True
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+        while x <= x1:
+            t = (x - x0) / (x1 - x0)
+            y = y0 * (1.0 - t) + y1 * t
+            if sleep:
+                self.set_pixel(x, y, colour)
+            else:
+                self.set_pixel(y, x, colour)
+            x += 1
+
+    # if x1 = 0 then we will not get any image because of (x1 - x0) < 0
+    def line_v_2(self, x1, y1, x0, y0, colour: Colour, delta_t):
+        x = x0
+        while x <= x1:
+            t = (x - x0) / (x1 - x0)
+            y = y0 * (1.0 - t) + y1 * t
+            self.set_pixel(x, y, colour)
+            x += 1
+
+    def task_9_print_triangle(self, x0, y0, z0, x1, y1, z1, x2, y2, z2, normals, index1, index2, index3):
+        xmin = float(min(x0, x1, x2))
+        ymin = float(min(y0, y1, y2))
+        xmax = float(max(x0, x1, x2))
+        ymax = float(max(y0, y1, y2))
+
+        # if (xmin < 0): xmin = 0
+        # if (ymin < 0): ymin = 0
+        # if (xmax > pic.h): xmax = pic.h
+        # if (ymax > pic.w): ymax = pic.w
+
+        n = np.cross([x1 - x0, y1 - y0, z1 - z0],
+                     [x1 - x2, y1 - y2, z1 - z2])
+
+        v = [0, 0, 1]
+        if (index1 >= len(normals) or index2 >= len(normals) or index3 >= len(normals)):
+            return
+        l0 = get_l(normals[index1], v)
+        l1 = get_l(normals[index2], v)
+        l2 = get_l(normals[index3], v)
+
+        cos_alpha = (n @ v) / np.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
+        if cos_alpha > 0:
+            return
+
+        color = Colour([255 * abs(cos_alpha), 0, 0])
+
+        for x in range(round(xmin), round(xmax)):
+            for y in range(round(ymin), round(ymax)):
+                lambdas = task_8_bara_sentral_coords(x, y, x0, y0, x1, y1, x2, y2)
+                brightness_value = 255 * (lambdas[0] * l0 + lambdas[1] * l1 + lambdas[2] * l2)
+                color = Colour([brightness_value, 0, 0])
+                if np.all(lambdas >= 0):
+                    z_val = lambdas[0] * z0 + lambdas[1] * z1 + lambdas[2] * z2
+                    if self.h > x > 0 and self.w > y > 0:
+                        if z_val > self.z_matrix[x][y]:
+                            self.z_matrix[x][y] = z_val
+                            self.set_pixel(x, y, color)
+                    else:
+                        continue
+
+    def line_v_1(self, x1, y1, x0, y0, delta_t=0.1):
+        t = 0.0
+        while t < 1.0:
+            x = x0 * (1.0 - t) + x1 * t
+            y = y0 * (1.0 - t) + y1 * t
+            self.set_pixel(x, y, self.default_colour)
+            t += delta_t
+
+    # функция отрисовки звезды из линий
+    def star_builder(self, variant, delta_t):
+        for i in range(13):
+            a = (2 * np.pi * i) / 13
+            variant(100 + 95 * np.cos(a), 100 + 95 * np.sin(a), 100, 100, delta_t)
+        self.show_picture()
 
 
 # считывание вершин с obj файла
@@ -107,87 +222,6 @@ def create_square_with_diff_colours(w, h):
     img.show()
 
 
-# функция отрисовки звезды из линий
-def star_builder(variant, delta_t, pic_star, pic_colour):
-    for i in range(13):
-        a = (2 * np.pi * i) / 13
-        variant(100 + 95 * np.cos(a), 100 + 95 * np.sin(a), 100, 100, pic_star, pic_colour, delta_t)
-    pic_star.show_picture()
-
-
-# exception if we go from last index to center
-def line_v_1(x1, y1, x0, y0, pic: Picture, colour: Colour, delta_t=0.1):
-    t = 0.0
-    while t < 1.0:
-        x = x0 * (1.0 - t) + x1 * t
-        y = y0 * (1.0 - t) + y1 * t
-        pic.set_pixel(x, y, colour)
-        t += delta_t
-
-
-# if x1 = 0 then we will not get any image because of (x1 - x0) < 0
-def line_v_2(x1, y1, x0, y0, pic: Picture, colour: Colour, delta_t):
-    x = x0
-    while x <= x1:
-        t = (x - x0) / (x1 - x0)
-        y = y0 * (1.0 - t) + y1 * t
-        pic.set_pixel(x, y, colour)
-        x += 1
-
-
-# 3-ий вариант отрисовки линий
-def line_v_3(x1, y1, x0, y0, pic: Picture, colour: Colour, delta_t=0.0):
-    sleep = False
-    x = x0
-    if abs(x0 - x1) < abs(y0 - y1):
-        x0, y0 = y0, x0
-        x1, y1 = y1, x1
-        sleep = True
-    if x0 > x1:
-        x0, x1 = x1, x0
-        y0, y1 = y1, y0
-    while x <= x1:
-        t = (x - x0) / (x1 - x0)
-        y = y0 * (1.0 - t) + y1 * t
-        if sleep:
-            pic.set_pixel(x, y, colour)
-        else:
-            pic.set_pixel(y, x, colour)
-        x += 1
-
-
-# 4-ый вариант отрисовки линий (алгоритм Брезенхема)
-def line_v_4(x1, y1, x0, y0, pic: Picture, colour: Colour, delts_t=0.0):
-    sleep = False
-    if abs(x0 - x1) < abs(y0 - y1):
-        x0, y0 = y0, x0
-        x1, y1 = y1, x1
-        sleep = True
-
-    if x0 > x1:
-        x0, x1 = x1, x0
-        y0, y1 = y1, y0
-
-    d_x = x1 - x0
-    d_y = y1 - y0
-    d_error = abs(d_y / float(d_x))
-    error = 0.
-    y = y0
-
-    for x in range(int(x0), int(x1) + 1):
-        if sleep:
-            pic.set_pixel(y, x, colour)
-        else:
-            pic.set_pixel(x, y, colour)
-        error += d_error
-        if error > 0.5:
-            if y1 > y0:
-                y += 1
-            else:
-                y -= 1
-            error -= 1.
-
-
 def task_1():
     # создание черного изображения
     create_coloured_square(512, 512, [0, 0, 0])
@@ -211,24 +245,24 @@ def task_3():
     # построение звезды 1-ым способом при дельта = 0.01
     # для последующих рисунков очищаем изображение, заполняя его дефолтным цветом
     delta_t = 0.01
-    star_builder(line_v_1, delta_t, pic, colour)
+    pic.star_builder(pic.line_v_1, delta_t)
     pic.clear()
 
     # построение звезды 1-ым способом при дельта = 0.1
     delta_t = 0.1
-    star_builder(line_v_1, delta_t, pic, colour)
+    pic.star_builder(pic.line_v_1, delta_t)
     pic.clear()
 
     # построение звезды 2-ым способом
-    star_builder(line_v_2, delta_t, pic, colour)
+    pic.star_builder(pic.line_v_2, delta_t)
     pic.clear()
 
     # построение звезды 3-им способом
-    star_builder(line_v_3, delta_t, pic, colour)
+    pic.star_builder(pic.line_v_3, delta_t)
     pic.clear()
 
     # построение звезды 4-ым способом
-    star_builder(line_v_4, delta_t, pic, colour)
+    pic.star_builder(pic.line_v_4, delta_t)
     pic.clear()
 
 
@@ -298,7 +332,7 @@ def task_5_6(multy, sum):
         # y2 = top_array[i_2 - 1][1] * multy + sum
         # z2 = top_array[i_2 - 1][2] * multy + sum
 
-        task_9_print_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, pic, normals, i_0, i_1, i_2)
+        pic.task_9_print_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, normals, i_0, i_1, i_2)
 
         # task_9_print_triangle(y0, x0, z0, y1, x1, z1, y2, x2, z2, pic)
     pic.show_picture()
@@ -323,48 +357,6 @@ def task_8_bara_sentral_coords(x, y, x0, y0, x1, y1, x2, y2):
     lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
     lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
     return np.array([lambda0, lambda1, lambda2])
-
-
-def task_9_print_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, pic: Picture, normals, index1, index2, index3):
-    xmin = float(min(x0, x1, x2))
-    ymin = float(min(y0, y1, y2))
-    xmax = float(max(x0, x1, x2))
-    ymax = float(max(y0, y1, y2))
-
-    # if (xmin < 0): xmin = 0
-    # if (ymin < 0): ymin = 0
-    # if (xmax > pic.h): xmax = pic.h
-    # if (ymax > pic.w): ymax = pic.w
-
-    n = np.cross([x1 - x0, y1 - y0, z1 - z0],
-                 [x1 - x2, y1 - y2, z1 - z2])
-
-    v = [0, 0, 1]
-    if (index1 >= len(normals) or index2 >= len(normals) or index3 >= len(normals)):
-        return
-    l0 = get_l(normals[index1], v)
-    l1 = get_l(normals[index2], v)
-    l2 = get_l(normals[index3], v)
-
-    cos_alpha = (n @ v) / np.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
-    if cos_alpha > 0:
-        return
-
-    color = Colour([255 * abs(cos_alpha), 0, 0])
-
-    for x in range(round(xmin), round(xmax)):
-        for y in range(round(ymin), round(ymax)):
-            lambdas = task_8_bara_sentral_coords(x, y, x0, y0, x1, y1, x2, y2)
-            brightness_value = 255 * (lambdas[0] * l0 + lambdas[1] * l1 + lambdas[2] * l2)
-            color = Colour([brightness_value, 0, 0])
-            if np.all(lambdas >= 0):
-                z_val = lambdas[0] * z0 + lambdas[1] * z1 + lambdas[2] * z2
-                if pic.h > x > 0 and pic.w > y > 0:
-                    if z_val > pic.z_matrix[x][y]:
-                        pic.z_matrix[x][y] = z_val
-                        pic.set_pixel(x, y, color)
-                else:
-                    continue
 
 
 def task_17(points):
@@ -411,8 +403,8 @@ def get_l(n, l_vector):
 
 
 if __name__ == '__main__':
-    # task_1()
-    # task_3()
-    task_5_6(7500, 7500)  # trooper
-    # task_5_6(5, 500) # fox
-    # task_5_6(100, 100)
+ #   task_1()
+    task_3()
+# task_5_6(7500, 7500)  # troop   er
+# task_5_6(5, 500) # fox
+# task_5_6(100, 100)
